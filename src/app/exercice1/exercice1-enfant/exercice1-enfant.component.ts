4b356e2c4f1b3e8159d5594f2bfb99596d07103a
d.chabbert <d.chabbert@ediser.com>
2021-04-10 16:38:42 +0200

    Done Exercice 1

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-exercice1-enfant',
-  templateUrl: './exercice1-enfant.component.html',
-  styleUrls: ['./exercice1-enfant.component.css']
+  selector: "app-exercice1-enfant",
+  templateUrl: "./exercice1-enfant.component.html",
+  styleUrls: ["./exercice1-enfant.component.css"],
 })
 export class Exercice1EnfantComponent implements OnInit {
+  @Input()
+  public counter: number;
 
-  constructor() { }
+  @Output()
+  private counterEventEmiter: EventEmitter<number> = new EventEmitter<number>();
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  public increaseOrDecreaseCounter(value: number): void {
+    this.counterEventEmiter.emit(this.counter + value);
   }
-
 }
